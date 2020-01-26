d7d840fce267da1398ebf5813c39603c316c68ad
glebster51 <glebster51@rambler.ru>
2020-01-26 22:17:25 +0800

    все работет, теперь пездато, набирать комбо слева направо

---
+++ Assets/G51/Script/HorizontalGrider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class HorizontalGrider : MonoBehaviour
+{
+    public float step;
+    private float w;
+#if UNITY_EDITOR
+    private void LateUpdate()
+    {
+        w = (transform.childCount - 1) * step;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var ch = transform.GetChild(i);
+            
+            ch.localPosition = Vector2.right * step * i - Vector2.right * w * 0.5f;
+        }
+    }
+#endif
+
+}
