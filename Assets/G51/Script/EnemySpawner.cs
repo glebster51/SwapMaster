f56e1fcbdbee488d306578993b4d3a5f79069779
glebster51 <glebster51@rambler.ru>
2020-01-26 22:44:53 +0800

    все нахуй иг8ра готова!!!

---
+++ Assets/G51/Script/EnemySpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float spawnWeight;
+    public Vector2 timerRamge;
+    public List<GameObject> enemys;
+
+    private List<Transform> spawned = new List<Transform>();
+    public float enemySpeed;
+
+
+    private void Start()
+    {
+        StartCoroutine(SpawnLoop(Random.Range(timerRamge.x, timerRamge.y)));
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < spawned.Count; i++)
+        {
+            Transform t = spawned[i];
+            if (t)
+                 t.position += Vector3.down * enemySpeed * Time.deltaTime;
+            else
+            {
+                spawned.RemoveAt(i);
+                i--;
+            }
+        }
+    }
+
+    IEnumerator SpawnLoop(float timeDelay)
+    {
+        yield return new WaitForSeconds(timeDelay);
+        Vector3 o = transform.position;
+        Vector3 r = Vector3.right * spawnWeight * 0.5f;
+        spawned.Add(Instantiate(enemys[Random.Range(0, enemys.Count)], transform.position + r * Random.Range(-1f, 1f), Quaternion.identity).transform);
+        StartCoroutine(SpawnLoop(Random.Range(timerRamge.x, timerRamge.y)));
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Vector3 o = transform.position;
+        Vector3 r = Vector3.right * spawnWeight * 0.5f;
+        Gizmos.DrawLine(o - r, o + r);
+    }
+}
